c35b41f6dc73bc5bdb3ee7f0cb7ccf58bc85c045
RuchithaPR <ruchithapr13@gmail.com>
2026-05-21 15:50:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,24 +10,31 @@ app.use(cors());
 app.use(express.json());
 
 // ---------- MONGODB CONNECTION ----------
-mongoose.connect('mongodb://ruchitha:ruchitha123@ac-dqbimx1-shard-00-00.wd8e7jl.mongodb.net:27017,ac-dqbimx1-shard-00-01.wd8e7jl.mongodb.net:27017,ac-dqbimx1-shard-00-02.wd8e7jl.mongodb.net:27017/libraryDB?ssl=true&replicaSet=atlas-8uzk9r-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0')
-.then(() => console.log('MongoDB Connected'))
-.catch((err) => console.log('MongoDB Error:', err));
+mongoose.connect('mongodb+srv://ruchitha:ruchitha123@cluster0.wd8e7jl.mongodb.net/libraryDB?retryWrites=true&w=majority')
+.then(() => {
+    console.log('MongoDB Connected');
+})
+.catch((err) => {
+    console.log('MongoDB Error:', err);
+});
 
 // ---------- ROUTES ----------
 const bookRoutes = require('./routes/bookRoutes');
+
 app.use(bookRoutes);
 
-// ---------- FRONTEND (optional for local testing) ----------
+// ---------- FRONTEND ----------
 app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.get('/', (req, res) => {
+
     res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
-// ---------- RENDER PORT FIX ----------
+// ---------- PORT ----------
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+
     console.log(`Server running on port ${PORT}`);
 });
