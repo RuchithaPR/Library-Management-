3ec3357885c4e0501ddcc804d62da19408091cbc
RuchithaPR <ruchithapr13@gmail.com>
2026-05-21 15:12:38 +0530

    Update bookRoutes.js

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,47 +1,112 @@
 const express = require('express');
+
 const router = express.Router();
 
 const Book = require('../models/Book');
 
+// ---------- GET ALL BOOKS ----------
 router.get('/books', async (req, res) => {
 
-    const books = await Book.find();
+    try {
 
-    res.json(books);
+        const books = await Book.find();
+
+        res.json(books);
+
+    } catch (error) {
+
+        res.status(500).json({
+            error: error.message
+        });
+    }
 });
 
+// ---------- ADD BOOK ----------
 router.post('/books', async (req, res) => {
 
-    const newBook = new Book(req.body);
+    try {
 
-    await newBook.save();
+        console.log(req.body);
 
-    res.json(newBook);
+        const { title, author, image } = req.body;
+
+        const newBook = new Book({
+            title,
+            author,
+            image
+        });
+
+        await newBook.save();
+
+        res.status(201).json(newBook);
+
+    } catch (error) {
+
+        res.status(500).json({
+            error: error.message
+        });
+    }
 });
 
+// ---------- BORROW BOOK ----------
 router.put('/books/borrow/:id', async (req, res) => {
 
-    await Book.findByIdAndUpdate(req.params.id, {
-        borrowed: true
-    });
+    try {
 
-    res.json({ message: 'Book Borrowed' });
+        await Book.findByIdAndUpdate(req.params.id, {
+            borrowed: true
+        });
+
+        res.json({
+            message: 'Book Borrowed'
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            error: error.message
+        });
+    }
 });
 
+// ---------- RETURN BOOK ----------
 router.put('/books/return/:id', async (req, res) => {
 
-    await Book.findByIdAndUpdate(req.params.id, {
-        borrowed: false
-    });
+    try {
 
-    res.json({ message: 'Book Returned' });
+        await Book.findByIdAndUpdate(req.params.id, {
+            borrowed: false
+        });
+
+        res.json({
+            message: 'Book Returned'
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            error: error.message
+        });
+    }
 });
 
+// ---------- DELETE BOOK ----------
 router.delete('/books/:id', async (req, res) => {
 
-    await Book.findByIdAndDelete(req.params.id);
+    try {
 
-    res.json({ message: 'Book Deleted' });
+        await Book.findByIdAndDelete(req.params.id);
+
+        res.json({
+            message: 'Book Deleted'
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            error: error.message
+        });
+    }
 });
 
 module.exports = router;
